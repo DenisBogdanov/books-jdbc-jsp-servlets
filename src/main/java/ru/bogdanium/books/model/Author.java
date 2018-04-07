7ae5f8aa7be20353f6e56f5fb6a2bc6b35cd9b80
Denis Bogdanov <bogdanov.math@gmail.com>
2018-04-07 10:36:12 +0300

    Add model.

---
+++ src/main/java/ru/bogdanium/books/model/Author.java
@@ -0,0 +1,64 @@
+package ru.bogdanium.books.model;
+
+import java.util.List;
+
+public class Author {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private List<Book> books;
+
+    public Author(String firstName, String lastName, List<Book> books) {
+        this(null, firstName, lastName, books);
+    }
+
+    public Author(Integer id, String firstName, String lastName, List<Book> books) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+}
